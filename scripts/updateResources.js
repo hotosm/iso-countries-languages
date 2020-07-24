2d4cdc0a86d8051156b54b4178401ebd5072e7f7
Wille Marcel <wille.yyz@gmail.com>
2020-07-24 08:46:47 -0300

    patch United States to United States of America

--- scripts/updateResources.js
+++ scripts/updateResources.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 
 import supportedLangs from '../src/supportedLangs';
 import countriesGeojson from '../res/countries';
+import patches from './patches';
 import { getCountryName, getCountryCode } from './utils';
 
 const countries = countriesGeojson.elements
@@ -11,6 +12,9 @@ const countries = countriesGeojson.elements
 for (const lang of supportedLangs) {
   const data = {};
   countries.forEach((country) => (data[getCountryCode(country)] = getCountryName(country, lang)));
+  if (patches[lang]) {
+    patches[lang].forEach((entry) => (data[entry.key] = entry.value));
+  }
   fs.writeFileSync(`./res/countries/${lang}.json`, JSON.stringify(data));
 }
 console.log('Finished!');
